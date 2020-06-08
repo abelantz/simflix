1cfa7a54b60bc53e5a91561876026e10aa293784
Abelant Zahiri <abelant@outlook.com>
2020-06-08 07:30:01 +0100

    refactoring

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -3,12 +3,13 @@
     <div class="container">
         <h2 class="title" >{{video[0].title}}</h2>
             <div class="player">
-                <vue-core-video-player controls :src="video[0].video" ></vue-core-video-player>
+                <vue-core-video-player id="videoElement" @canplay="updatePaused" @playing="updatePaused" @pause="updatePaused" controls :src="video[0].video" ></vue-core-video-player>
+                <div class="controls">
+                    <button v-show="paused" @click="play">&#9654;</button>
+                    <button v-show="playing" @click="pause">&#9208;</button>
+                </div>
             </div>
-        <div class="controls">
-            <button v-show="paused" @click="play" >&#9654;</button>
-            <button v-show="playing" @click="pause">&#9208;</button>
-        </div>
+        
     </div>
 </template>
 <script>
@@ -21,12 +22,25 @@ export default {
             paused: null
         }
     },
+    methods: {
+    updatePaused(event) {
+      this.videoElement = event.target;
+      this.paused = event.target.paused;
+    },
+    play() {
+      this.videoElement.play();
+    },
+    pause() {
+      this.videoElement.pause();
+    }
+  },
     computed:{
         video(){
             return this.$store.state.videos.filter(video => {
                 return video.title === this.vId
             })
         },
+        playing() { return !this.paused; }
     },
 }
 </script>
